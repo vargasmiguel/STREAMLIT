7a2381d3e1d598885362af9d5bf31d8aa6b22e10
vargasmiguel <54147317+vargasmiguel@users.noreply.github.com>
2023-06-14 20:03:16 -0500

    hola2

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ st.title('📷 Cámara 📷')
 pic=st.camera_input('Usa la :red[cámara] para tomar una :green[foto] $\sum$')
 
 if pic:
-    st.image(pic)
+    st.balloons()
+    st.write("¡Que buena toma!")
 
 st.markdown("By Miguel y P$\\alpha$sc$\\alpha$l")
 
